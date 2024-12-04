9688a0e58b527b798f0bc06c2ad4de357d7931dd
David Szen <d.szen@siteco.de>
2024-12-04 09:55:46 +0100

    registererror

---
+++ GenericDataStore/GenericDataStore/InputModels/TextGeneratorService.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using Microsoft.ML;
+
+using Microsoft.ML.Data;
+
+
+
+namespace CodeGenerationML
+
+{
+
+    public class Program
+
+    {
+
+        // Bemeneti adat osztálya
+
+        public class InputData
+
+        {
+
+            [LoadColumn(0)]
+
+            public string Keywords { get; set; }
+
+
+
+            [LoadColumn(1)]
+
+            public string Fields { get; set; }
+
+
+
+            [LoadColumn(2)]
+
+            public int Flag { get; set; }
+
+        }
+
+
+
+        // Kimeneti adat osztálya
+
+        public class GeneratedCode
+
+        {
+
+            public string OutputCode { get; set; }
+
+        }
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            var mlContext = new MLContext();
+
+
+
+            // Példa adatok feltöltése
+
+            var trainingData = new List<InputData>
+
+            {
+
+                new InputData { Keywords = "chart, graph", Fields = "Sales, Year", Flag = 1 },
+
+                new InputData { Keywords = "color, palette", Fields = "Theme, Hue", Flag = 2 },
+
+                new InputData { Keywords = "column, calculation", Fields = "Price, Tax", Flag = 3 }
+
+            };
+
+
+
+            // Training Data betöltése
+
+            var trainingDataView = mlContext.Data.LoadFromEnumerable(trainingData);
+
+
+
+            // Text featurization pipeline
+
+            var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText("KeywordsFeaturized", nameof(InputData.Keywords))
+
+                .Append(mlContext.Transforms.Text.FeaturizeText("FieldsFeaturized", nameof(InputData.Fields)))
+
+                .Append(mlContext.Transforms.Concatenate("Features", "KeywordsFeaturized", "FieldsFeaturized"));
+
+
+
+            // Regresszor modell (kód generáláshoz egyszerű regressziót használunk itt)
+
+            var trainer = mlContext.Transforms.Conversion.MapValueToKey("Label", nameof(InputData.Flag))
+
+                .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy())
+
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+
+
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+
+
+            // Modell betanítása
+
+            var model = trainingPipeline.Fit(trainingDataView);
+
+
+
+            // Predikciós adatok
+
+            var testData = new List<InputData>
+
+            {
+
+                new InputData { Keywords = "chart, bar", Fields = "Revenue, Month", Flag = 1 },
+
+                new InputData { Keywords = "gradient, shade", Fields = "Color, Intensity", Flag = 2 },
+
+                new InputData { Keywords = "aggregate, column", Fields = "Quantity, Discount", Flag = 3 }
+
+            };
+
+
+
+            var testDataView = mlContext.Data.LoadFromEnumerable(testData);
+
+
+
+            // Predikció
+
+            var predictions = model.Transform(testDataView);
+
+            var predictedResults = mlContext.Data.CreateEnumerable<GeneratedCode>(predictions, reuseRowObject: false);
+
+
+
+            // Eredmények kiíratása
+
+            Console.WriteLine("Generated Code Predictions:");
+
+            foreach (var prediction in predictedResults)
+
+            {
+
+                Console.WriteLine($"Generated Code: {prediction.OutputCode}");
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ GenericDataStore/GenericDataStore/Services/TextToCalculation.cs
@@ -0,0 +1,147 @@
+﻿using GenericDataStore.Models;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+
+namespace GenericDataStore.Services
+{
+    public class TextToCalculation
+    {
+        public List<string> ChartKeys = new List<string>()
+        {
+            "#chart",
+            "chart",
+            "plot",
+            "diagram",
+            "graph"
+        };
+
+        public List<string> FieldKeys = new List<string>
+        {
+            "#field",
+            "field",
+            "column",
+            "row",
+            "cell",
+        };
+
+        public List<string> ColorKeys = new List<string>
+        {
+            "#color",
+            "color",
+            "colour",
+            "colored",
+            "coloured",
+            "background",
+            "backgroundcolor"
+        };
+
+
+        public int CalculateLevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Inicializálás  
+            for (int i = 0; i <= n; i++)
+                d[i, 0] = i;
+            for (int j = 0; j <= m; j++)
+                d[0, j] = j;
+
+            // Elementorozza a táblázatot  
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
+        }
+
+        public void TranslateToKeys(string text, ObjectType obj)
+        {
+            List<string> words = text.Split(' ').ToList();
+            List<string> keywords = new List<string>();
+            List<Field> mentionedfields = new List<Field>();
+            foreach (var item in words)
+            {
+                foreach (string keyword in Keywords)
+                {
+                    if (CalculateLevenshteinDistance(item.ToLower(), keyword) < 2)
+                    {
+                        keywords.Add(keyword);
+                    }
+                }
+
+                foreach (var field in obj.Field)
+                {
+                    if (CalculateLevenshteinDistance(item.ToLower(), field.Name.ToLower()) < 2)
+                    {
+                        mentionedfields.Add(field);
+                    }
+                }
+            }
+
+        }
+
+        //public List<Value> GetValueKeywords(List<string> words, List<Field> mentionedfields, ObjectType obj)
+        //{
+        //    List<string> valuewords = new List<string>();
+        //    List<bool>
+        //    foreach (var word in words)
+        //    {
+        //        bool result;
+        //        if(bool.TryParse(word, out result) == true)
+        //        {
+                    
+        //        }
+        //        foreach (var item2 in mentionedfields)
+        //        {
+        //            foreach (var item in obj.DataObject)
+        //            {
+        //                var value = item.Value.FirstOrDefault(x => x.Name == item2.Name);
+
+        //                    if (CalculateLevenshteinDistance(word.ToLower(), value.ValueString.ToLower()) < 2)
+        //                    {
+        //                        valuewords.Add(value.ValueString);
+        //                    }
+                        
+        //            }
+        //        }
+        //    }
+
+        //    return valuewords;
+        //}
+
+        public string GetDescFromKeys(List<string> keys)
+        {
+            if (keys.Contains("chart") || keys.Contains("plot") || keys.Contains("diagram") || keys.Contains("graph"))
+            {
+
+            }
+            else if (keys.Contains("field") || keys.Contains("column") || keys.Contains("row") || keys.Contains("cell"))
+            {
+
+            }
+            else if ((keys.Contains("background") || keys.Contains("backgroundcolor") || keys.Contains("color") || keys.Contains("colour") || keys.Contains("colored") || keys.Contains("coloured"))
+            {
+
+            }
+
+        }
+
+        public string CartCalculation(List<string> keys, List<Field> mentionedfields)
+        {
+            if(keys.Contains("x") && keys.Contains("y"))
+            {
+                if(keys.IndexOf("x") < keys.IndexOf("y"))
+                {
+
+                }
+                
+            }
+        }
+    }
+}
